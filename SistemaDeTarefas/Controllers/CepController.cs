b34512ffee705b836285c6ee2a7809e91a9b8894
Acaciano Neves Souza <acaciano.neves@gmail.com>
2023-07-05 20:53:53 -0300

    Api Via CEP

---
+++ SistemaDeTarefas/Controllers/CepController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Integracao.Interfaces;
+using SistemaDeTarefas.Integracao.Response;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        private readonly IViaCepIntegracao _viaCepIntegracao;
+
+        public CepController(IViaCepIntegracao viaCepIntegracao)
+        {
+            _viaCepIntegracao = viaCepIntegracao;
+        }
+
+        [HttpGet("{cep}")]
+        public async Task<ActionResult<ViaCepResponse>> ListarDadosEndereco(string cep)
+        {
+            var responseData = await _viaCepIntegracao.ObterDadosViaCep(cep);
+
+            if(responseData == null)
+            {
+                return BadRequest("CEP não encotrado!");
+            }
+
+            return Ok(responseData);
+        }
+    }
+}
